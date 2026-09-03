914deeef3992d197bf30e4545fe774d78bbb176c
VishalSinoju <vsingoju17@gmail.com>
2026-09-04 00:03:51 +0530

    edited some code

---
+++ next.config.js
@@ -0,0 +1,14 @@
+{import('next').NextConfig} 
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',  
+        pathname: '/**',                 
+      },
+    ],
+  },
+
+ 
+};
